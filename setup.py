01862be8c4a95a97fdd90f63404f47c7a22eaa09
Duologic <jeroen.opteynde@mobilevikings.com>
2013-07-23 15:26:19 +0200

    added simple setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(name='ranchy',
+      description='Ranchy project will be able to collect data about a server farm',
+      long_description=open('README.rst', 'r').read()
+      author='Jeroen Op \'t Eynde',
+      author_email='jeroen.opteynde@mobilevikings.com',
+      url='http://github.com/Duologic/ranchy',
+      packages=['ranchy'],
+     )

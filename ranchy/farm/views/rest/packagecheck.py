4b1bb9e93e604cf7763db105324a8a638b824a68
Jeroen Op 't Eynde <jeroen.opteynde@mobilevikings.com>
2013-08-12 12:19:18 +0200

    pagination of packagecheck

--- ranchy/farm/views/rest/packagecheck.py
+++ ranchy/farm/views/rest/packagecheck.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from django.http import HttpResponse
 
 from farm.models import PackageCheck
 from farm.serializers import PackageCheckSerializer
@@ -7,19 +6,14 @@ from farm.serializers import PackageCheckSerializer
 
 class PackageCheckList(generics.ListCreateAPIView):
     serializer_class = PackageCheckSerializer
+    paginate_by = 100
 
     def get_queryset(self):
         queryset = PackageCheck.objects.all()
-        filtered = False
         if "nodeslug" in self.kwargs:
             queryset = queryset.filter(node__slug=self.kwargs['nodeslug'])
-            filtered = True
         if "packageid" in self.kwargs:
             queryset = queryset.filter(package=self.kwargs['packageid'])
-            filtered = True
-        if not filtered:
-            content = {'queryset','query too long'}
-            return HttpResponse(content, status=501)
 
         return queryset
 
